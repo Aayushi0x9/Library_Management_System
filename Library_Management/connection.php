bd26e8579e225025a9843f92675c4ae4d8cfbef0
Aayushi Nikhare <143987694+Aayushi0x9@users.noreply.github.com>
2024-11-26 23:06:26 +0530

    Add files via upload

---
+++ Library_Management/connection.php
@@ -0,0 +1,8 @@
+<?php
+          $server = 'localhost';
+          $username='root';
+          $password='';
+          $dbname='library_mangement';
+          $conn = mysqli_connect($server,$username,$password,$dbname);
+      
+?>
