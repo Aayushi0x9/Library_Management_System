908016ebd9d33b3ca8980bea0610462639f03f39
Aayushi Nikhare <143987694+Aayushi0x9@users.noreply.github.com>
2024-11-27 13:01:01 +0530

    Delete Library_Management directory

--- Library_Management/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-          $server = 'localhost';
-          $username='root';
-          $password='';
-          $dbname='library_mangement';
-          $conn = mysqli_connect($server,$username,$password,$dbname);
-      
-?>
